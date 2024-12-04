21692cfff80ab7a6339e9b37ce331c4cd192c73c
mdasif8393 <md.asif.8393@gmail.com>
2024-12-04 22:34:16 +0600

    Test Academic Faculty Routes Using Postman

---
+++ src/app/modules/academicFaculty/academicFaculty.service.ts
@@ -0,0 +1,34 @@
+import { TAcademicFaculty } from './academicFaculty.interface';
+import { AcademicFaculty } from './academicFaculty.model';
+
+const createAcademicFacultyIntoDB = async (payload: TAcademicFaculty) => {
+  const result = await AcademicFaculty.create(payload);
+  return result;
+};
+
+const getAllAcademicFacultiesFromDB = async () => {
+  const result = await AcademicFaculty.find({});
+  return result;
+};
+
+const getSingLeAcademicFacultyFromDB = async (_id: string) => {
+  const result = await AcademicFaculty.findById(_id);
+  return result;
+};
+
+const updateAcademicFaculty = async (
+  id: string,
+  payload: Partial<TAcademicFaculty>,
+) => {
+  const result = await AcademicFaculty.findOneAndUpdate({ _id: id }, payload, {
+    new: true,
+  });
+  return result;
+};
+
+export const AcademicFacultyServices = {
+  createAcademicFacultyIntoDB,
+  getAllAcademicFacultiesFromDB,
+  getSingLeAcademicFacultyFromDB,
+  updateAcademicFaculty,
+};
--- src/app/modules/academicFaculty/academicFaculty.validation.ts
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,14 +1,25 @@
 import { z } from 'zod';
 
-const createAcademicFaculty = z.object({
+const createAcademicFacultyValidationSchema = z.object({
   body: z.object({
     name: z.string({
-      invalid_type_error: 'Academic Faculty Name must be a string',
-      required_error: 'Academic Faculty Name is required',
+      invalid_type_error: 'Academic Faculty must be a string',
+      required_error: 'Academic Faculty is required',
     }),
   }),
 });
 
+const updateAcademicFacultyValidationSchema = z.object({
+  body: z.object({
+    name: z
+      .string({
+        invalid_type_error: 'Academic Faculty must be a string',
+      })
+      .optional(),
+  }),
+});
+
 export const AcademicFacultyValidations = {
-  createAcademicFaculty,
+  createAcademicFacultyValidationSchema,
+  updateAcademicFacultyValidationSchema,
 };
