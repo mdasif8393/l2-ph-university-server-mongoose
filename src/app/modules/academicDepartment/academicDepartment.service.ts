e9172d33a501ca81d49cdcd6f6c90b3eef54e87f
mdasif8393 <md.asif.8393@gmail.com>
2024-12-04 23:49:08 +0600

    complete academic department

---
+++ src/app/modules/academicDepartment/academicDepartment.service.ts
@@ -0,0 +1,38 @@
+import { TAcademicDepartment } from './academicDepartment.interface';
+import { AcademicDepartment } from './academicDepartment.model';
+
+const createAcademicDepartmentIntoDB = async (payload: TAcademicDepartment) => {
+  const result = await AcademicDepartment.create(payload);
+  return result;
+};
+
+const getAllAcademicDepartmentsFromDB = async () => {
+  const result = await AcademicDepartment.find({});
+  return result;
+};
+
+const getSingleAcademicDepartmentFromDB = async (_id: string) => {
+  const result = await AcademicDepartment.findById(_id);
+  return result;
+};
+
+const updateAcademicDepartment = async (
+  id: string,
+  payload: Partial<TAcademicDepartment>,
+) => {
+  const result = await AcademicDepartment.findOneAndUpdate(
+    { _id: id },
+    payload,
+    {
+      new: true,
+    },
+  );
+  return result;
+};
+
+export const AcademicDepartmentServices = {
+  createAcademicDepartmentIntoDB,
+  getAllAcademicDepartmentsFromDB,
+  getSingleAcademicDepartmentFromDB,
+  updateAcademicDepartment,
+};
---
+++ src/app/modules/academicDepartment/academicDepartment.validation.ts
@@ -0,0 +1,34 @@
+import { z } from 'zod';
+
+const createAcademicDepartmentValidationSchema = z.object({
+  body: z.object({
+    name: z.string({
+      required_error: 'Academic Department is required',
+      invalid_type_error: 'Academic Department must be a string',
+    }),
+    academicFaculty: z.string({
+      required_error: 'academicFaculty reference id is required',
+      invalid_type_error: 'academicFaculty reference id must be a string',
+    }),
+  }),
+});
+
+const updateAcademicDepartmentValidationSchema = z.object({
+  body: z.object({
+    name: z
+      .string({
+        invalid_type_error: 'Academic Department must be a string',
+      })
+      .optional(),
+  }),
+  academicFaculty: z
+    .string({
+      invalid_type_error: 'academicFaculty reference id must be a string',
+    })
+    .optional(),
+});
+
+export const AcademicDepartmentValidations = {
+  createAcademicDepartmentValidationSchema,
+  updateAcademicDepartmentValidationSchema,
+};
