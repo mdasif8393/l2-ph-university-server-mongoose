19e421656c1278a903eee8999504774eeaad78a3
mdasif8393 <md.asif.8393@gmail.com>
2024-12-04 14:59:36 +0600

    add dynamic student id

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -16,13 +16,30 @@ const findLastStudentId = async () => {
     .sort({ createdAt: -1 })
     .lean();
   // example: From 2030010001 return 0001
-  return lastStudent?.id ? lastStudent?.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent?.id : undefined;
 };
 
 // 2030 01 0000
 export const generateStudentId = async (payload: TAcademicSemester) => {
   // first time 0000
-  const currentId = (await findLastStudentId()) || (0).toString();
+  let currentId = (0).toString(); // 0000 by default
+
+  // 2030 01 0001
+  const lastStudentId = await findLastStudentId();
+
+  const lastStudentYear = lastStudentId?.substring(0, 4);
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+  const currentSemesterCode = payload.code;
+  const currentYear = payload.year;
+
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear == currentYear
+  ) {
+    currentId = lastStudentId?.substring(6);
+  }
+
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
   incrementId = `${payload.year}${payload.code}${incrementId}`;
   return incrementId;
