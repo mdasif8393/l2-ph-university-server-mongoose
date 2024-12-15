7d265950f11324a5efae4aac4cac3b64b7e60c9e
mdasif8393 <md.asif.8393@gmail.com>
2024-12-15 16:27:32 +0600

    17-5 Create Auth Middleware

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -1,20 +1,17 @@
 import { NextFunction, Request, Response } from 'express';
 import { AnyZodObject } from 'zod';
+import catchAsync from '../utils/catchAsync';
 
 const validateRequest = (schema: AnyZodObject) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      // validation check
-      // if everything all right next()
-      await schema.parseAsync({
-        body: req.body,
-      });
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    // validation check
+    // if everything all right next()
+    await schema.parseAsync({
+      body: req.body,
+    });
 
-      return next();
-    } catch (err) {
-      next(err);
-    }
-  };
+    return next();
+  });
 };
 
 export default validateRequest;
